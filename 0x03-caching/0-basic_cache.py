0244683ea6587448422f8dc61b0596451a237829
Jebeniani <aymenjebeniani05@gmail.com>
2023-01-27 19:16:33 +0100

    readme file

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,10 +7,12 @@ class BasicCache(BaseCaching):
     """Caching system"""
 
     def put(self, key, item):
+        """Caching system"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Caching system"""
         if key is not None:
             for k, v in self.cache_data.items():
                 if k == key:
