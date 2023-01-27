1faffddf32fb92b36b3c376306a0dc7a23ff52d2
Jebeniani <aymenjebeniani05@gmail.com>
2023-01-27 19:25:17 +0100

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""BaseCaching Caching System"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO caching system"""
+
+    def __init__(self):
+        """Initialize cache attributes"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to the cache"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            print("DISCARD:", list(self.cache_data.keys())[0])
+            del self.cache_data[list(self.cache_data.keys())[0]]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
