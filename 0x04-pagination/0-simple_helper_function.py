d733719d49d79069be767cadcb9918a768997adf
Jebeniani <aymenjebeniani05@gmail.com>
2023-01-31 11:52:54 +0100

    index range

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -4,6 +4,10 @@ two integer arguments page and page_size"""
 
 
 def index_range(page, page_size):
+    """the function return a tuple of size two
+    containing a start index and an end index corresponding
+    to the range of indexes to return in a list
+    for those particular pagination parameters."""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
