a70c4d838a00e073d94fe4fc3d0bfcc6b9bee2f9
Jebeniani <aymenjebeniani05@gmail.com>
2023-02-03 18:59:38 +0100

    format method implementation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ function that returns the log message obfuscated """
     for i in fields:
-        message = re.sub(f'{i}=(.*?)\{separator}',
+        message = re.sub(rf'{i}=(.*?)\{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
 
@@ -33,4 +33,5 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """an updated method to filter values in the incoming log records"""
         return filter_datum(
-            self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+            self.fields, self.REDACTION,
+            super().format(record), self.SEPARATOR)
