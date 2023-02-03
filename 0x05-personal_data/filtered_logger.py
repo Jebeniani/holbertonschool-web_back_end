d0faa76e4d38ec396f30c3e4982a451662b8d5c9
Jebeniani <aymenjebeniani05@gmail.com>
2023-02-03 17:49:14 +0100

    obfuscated log msg

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+import re
+
+
+def filter_datum(fields: list[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ function that returns the log message obfuscated """
+    for i in fields:
+        message = re.sub(f'{i}=.*?{separator}',
+                         f'{i}={redaction}{separator}', message)
+    return message
