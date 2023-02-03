4bba263212a38d85ad6e530273ec43f7641ae2cc
Jebeniani <aymenjebeniani05@gmail.com>
2023-02-03 18:04:29 +0100

    obfuscated log msg

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 import re
+from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password",)
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ function that returns the log message obfuscated """
     for i in fields:
