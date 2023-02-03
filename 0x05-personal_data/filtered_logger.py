d299b5582be032726a074d92852a5661577d1ca3
Jebeniani <aymenjebeniani05@gmail.com>
2023-02-03 18:57:16 +0100

    format method implementation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Regex-ing"""
+"""filtered_logger"""
 import re
 from typing import List
+import logging
+
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password",)
 
@@ -10,6 +12,25 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ function that returns the log message obfuscated """
     for i in fields:
-        message = re.sub(f'{i}=.*?{separator}',
+        message = re.sub(f'{i}=(.*?)\{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializer"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """an updated method to filter values in the incoming log records"""
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
