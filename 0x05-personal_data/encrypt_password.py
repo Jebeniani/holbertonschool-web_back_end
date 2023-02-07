1c191c918b67478b6a3426d5d1fbef51c4a2b21f
Jebeniani <aymenjebeniani05@gmail.com>
2023-02-07 11:35:35 +0100

    Encrypting passwords

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """ function that expects one string argument name password
     and returns a salted, hashed password, which is a byte string"""
     salt = bcrypt.gensalt()
