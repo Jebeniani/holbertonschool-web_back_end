9732c4b7a2ce57b693d5962b51157f1d5bc6e5a1
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 23:52:06 +0100

    creating a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-"""Empty session"""
+"""creating a session"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """empty empty"""
-    pass
+    """updating sessionauth class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """method that creates a session idfor a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
