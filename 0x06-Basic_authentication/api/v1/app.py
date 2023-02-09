dfb57ea70a6e41a6e01a37b47ce19e20abb90dbb
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 03:21:41 +0100

    Unauthorized err handler

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -14,11 +14,11 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(error) -> str:
     """ Not found handler
     """
-    return jsonify({"error": "Not found"}), 404
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+  """adding a new endpoint for testing this new error handler"""
+  abort(401)
