6cd960225132c34259d0a29e3359840478bef1e2
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 04:42:22 +0100

    which routes

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -6,9 +6,20 @@ from typing import List, TypeVar
 
 class Auth():
     """api authentication manager"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Return False - path and excluded_paths will be used later"""
-        return False
+        class Auth:
+            def require_auth(self, path: str, excluded_paths:
+                             List[str]) -> bool:
+                if not path:
+                    return True
+        if not excluded_paths or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path == excluded_path or path.startswith(excluded_path + '/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return None - request will be the Flask request object"""
