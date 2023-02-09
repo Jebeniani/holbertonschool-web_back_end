a3fee4a18accdad7c07526e867d9d204155ec2b0
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 23:44:18 +0100

    Empty session

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.session_auth import SessionAuth
+
 
 
 app = Flask(__name__)
@@ -19,6 +21,8 @@ if os.getenv("AUTH_TYPE"):
 if os.getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Empty session"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """empty empty"""
+    pass
