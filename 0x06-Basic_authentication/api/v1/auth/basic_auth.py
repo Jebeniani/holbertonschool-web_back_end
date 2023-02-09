c3f314530537816620fae0390891ab8d6a3c31c1
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 21:19:21 +0100

    user credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,16 @@ class BasicAuth(Auth):
             return decodedValue
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(":", 1)
+        return credentials[0], credentials[1]
