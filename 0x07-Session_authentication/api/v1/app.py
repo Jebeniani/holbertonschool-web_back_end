4c7c2205c2463a123a36e17cc5d2ddb94eef9d42
jebeniani <aymenjebeniani@gmail.com>
2023-02-12 20:54:12 +0100

    before request update

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -48,10 +48,11 @@ def before_request() -> str:
         return
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if not auth.authorization_header(request) and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
