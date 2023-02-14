316770f53b18cd1cb7557199dc0cf590523958a9
jebeniani <aymenjebeniani@gmail.com>
2023-02-14 13:14:39 +0100

    bcrypt use

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     and returns a salted, hashed password, which is a byte string"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """function that expects 2 arguments and returns a boolean"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
