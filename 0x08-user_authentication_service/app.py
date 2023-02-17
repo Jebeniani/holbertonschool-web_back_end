78b67872adacb2bc8aa7ddfae931459e0cf67be7
jebeniani <aymenjebeniani@gmail.com>
2023-02-17 21:49:58 +0100

    LOGOUT

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -49,5 +49,14 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """logout function"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
