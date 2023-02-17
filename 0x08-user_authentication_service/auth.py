7b2d1fe188dd18cd6e17d171c0f24cdc03a2795d
jebeniani <aymenjebeniani@gmail.com>
2023-02-17 22:46:43 +0100

    updating hash hash

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -78,3 +78,13 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updating the user's hashed pwd field with
+        the new hashed password and the reset_token fieldto None"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
