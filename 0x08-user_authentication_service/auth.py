f8029f2f02fd2f44cbf0bffd73ad8a01a46141f0
jebeniani <aymenjebeniani@gmail.com>
2023-02-17 15:38:16 +0100

    credentials validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -30,3 +30,11 @@ class Auth:
             return user
         else:
             raise ValueError(f"User {email} already exists.")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """locating the user by email"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
