414c7f64227b225f20477fe0e7a47e5e6a6cc387
jebeniani <aymenjebeniani@gmail.com>
2023-02-17 23:01:22 +0100

    Updating pwd end-point

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -81,5 +81,18 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """updating a user's âssword via PUT request to the end point"""
+    email = request.form.get('email')
+    token = request.form.get('token')
+    pwd = request.form.get('password')
+    try:
+        AUTH.update_password(token, email, pwd)
+    except Exception:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
