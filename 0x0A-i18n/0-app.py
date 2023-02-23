86594b1b475ab4026dfa2cec2d4033418ee31a1d
jebeniani <aymenjebeniani@gmail.com>
2023-02-23 14:42:47 +0100

    basic flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
     """basic index"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
