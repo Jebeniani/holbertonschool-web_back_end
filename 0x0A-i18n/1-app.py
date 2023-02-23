7a000582679fb5906303fd4eb9b0b7518226936c
jebeniani <aymenjebeniani@gmail.com>
2023-02-23 15:10:33 +0100

    basic Babel

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """basic index"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
