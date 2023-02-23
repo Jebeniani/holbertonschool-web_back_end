5603404e00d8949818bd1fd7497ff6fcfe3b4a2e
jebeniani <aymenjebeniani@gmail.com>
2023-02-23 14:31:16 +0100

    basic flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
