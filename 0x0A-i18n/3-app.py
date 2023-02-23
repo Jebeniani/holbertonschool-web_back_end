9bf9cef151b4a3e3333a3517514ee3976c566e9b
jebeniani <aymenjebeniani@gmail.com>
2023-02-23 16:22:29 +0100

    parametrize templates

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Get locale from request
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from babel import negotiate_locale
+
+app = Flask(__name__, template_folder='templates')
+babel = Babel(app)
+
+
+class Config(object):
+    """Config Class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """basic index"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the best matched locale based on the languages
+    requested by the client"""
+    return negotiate_locale('LANGUAGES',
+                            request.accept_languages.best_match(
+                                app.config['LANGUAGES']))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
