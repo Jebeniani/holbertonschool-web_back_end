7c66a6326bffa4709f47e631109ad5d961c8e60d
jebeniani <aymenjebeniani@gmail.com>
2023-03-07 17:37:37 +0100

    recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from uuid import uuid4
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,29 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            bytes, str, int, float, None]:
+        """
+        Retrieves the data associated with the given key from Redis
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[bytes, str, None]:
+        """
+        Retrieves the data associated with the given key
+        from Redis and attempts to convert it to a string
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[bytes, int, None]:
+        """
+        Retrieves the data associated with the given key
+        from Redis and attempts to convert it to an integer
+        """
+        return self.get(key, fn=lambda d: int(d))
