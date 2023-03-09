bf1c23316dd4cde54bf99da6f7e1b8fa28b5909a
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:41:33 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- creating a table users with specified requirements
+-- is it possible to
 SELECT DISTINCT `band_name`,
                 IFNULL(`split`, 2020) - `formed` as `lifespan`
   FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
