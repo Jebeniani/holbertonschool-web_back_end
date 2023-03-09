ed2e49b6b445a134814cb192f489898a15623543
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:30:36 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- creating a table users with specified requirements
-SELECT band_name, IFNULL(split, 2020) - formed as lifespan
+SELECT DISTINCT band_name, IFNULL(split, 2020) - formed as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%';
