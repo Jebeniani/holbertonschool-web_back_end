1ae019fb0c0a2c133627df185461c04ec548fbaf
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:48:37 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- creating a table users with specified requirements
 -- is it possible to
-SELECT `band_name`, IFNULL(`split`, 2020) - IFNULL(formed, 0) as `lifespan`
+SELECT `band_name`, IFNULL(`split`, 2023) - IFNULL(formed, 0) as `lifespan`
 FROM `metal_bands` WHERE style LIKE '%Glam rock%';
