807e5cf1dc79592e89fbaecad9cfdb87a1cb82d0
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:47:29 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- creating a table users with specified requirements
 -- is it possible to
 SELECT `band_name`, IFNULL(`split`, 2020) - IFNULL(formed, 0) as `lifespan`
-FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style);
+FROM `metal_bands` WHERE style LIKE '%Glam rock%';
