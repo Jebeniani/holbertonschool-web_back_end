9a105d1934f963b7b1f461cd607eba11cb640e8b
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:17:45 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,3 @@
-SELECT band_name, (YEAR(split) - YEAR(formed)) AS lifespan
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
