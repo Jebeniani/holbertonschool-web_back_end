c3873817b0ceaa0e56d0f8e34d35f65015c200c4
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:39:10 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- creating a table users with specified requirements
-SELECT DISTINCT 'band_name',
-IFNULL('split', 2020) - 'formed' as 'lifespan'
-FROM 'metal_bands' WHERE FIND_IN_SET('Glam rock', style)
-ORDER BY 'lifespan' DESC;
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2020) - `formed` as `lifespan`
+  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+  ORDER BY `lifespan` DESC;
