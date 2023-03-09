c71b98fe368ec0a81e9487a38a4917cb6210fdf6
jebeniani <aymenjebeniani@gmail.com>
2023-03-09 23:44:14 +0100

    old school homie

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- creating a table users with specified requirements
 -- is it possible to
-SELECT DISTINCT `band_name`,
-                IFNULL(`split`, 2020) - `formed` as `lifespan`
-  FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
-  ORDER BY `lifespan` DESC;
+SELECT `band_name`, IFNULL(`split`, 2020) - IFNULL(formed, 0) as `lifespan`
+FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+ORDER BY `lifespan` DESC;
