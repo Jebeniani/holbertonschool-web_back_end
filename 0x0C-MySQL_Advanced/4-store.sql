9f6c553f7e2ceec52ee9039a404bfe79db066c50
jebeniani <aymenjebeniani@gmail.com>
2023-03-10 13:03:19 +0100

    pen pineapple apple pen

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,4 +1,4 @@
 -- creating a table users with specified requirements
 CREATE TRIGGER decrement AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_id;
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
