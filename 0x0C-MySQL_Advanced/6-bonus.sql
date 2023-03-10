7840a71dd88c73f894099c0a5535320bb305ec51
jebeniani <aymenjebeniani@gmail.com>
2023-03-10 14:08:18 +0100

    add bonus

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score FLOAT
+)
+BEGIN
+    DECLARE project_id INT;
+
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
+
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+END
+$$
