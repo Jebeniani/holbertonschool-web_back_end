77e2a71348f8c6f79c1c76b9ec0c5c5925c260de
jebeniani <aymenjebeniani@gmail.com>
2023-03-10 13:56:58 +0100

    email validation

--- 0x0C-MySQL_Advanced/5-valid_email.sql
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,5 @@
 -- creating a table users with specified requirements
+DELIMITER $$
 CREATE TRIGGER email_validation
 BEFORE UPDATE ON users
 FOR EACH ROW
@@ -6,4 +7,5 @@ BEGIN
     IF NEW.email != OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
-END;
+END
+$$
