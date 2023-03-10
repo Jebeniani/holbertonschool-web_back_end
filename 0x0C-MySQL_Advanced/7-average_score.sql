1b53d90cd3015bbe55389a36a71f27c6cdb7a0a0
jebeniani <aymenjebeniani@gmail.com>
2023-03-10 14:45:12 +0100

    Average score

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- creating a table users with specified requirements
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+UPDATE users
+SET average_score = 
+(SELECT AVG(score)
+FROM corrections
+WHERE corrections.user_id=user_id
+GROUP BY corrections.user_id)
+WHERE id=user_id;
+END
+$$
