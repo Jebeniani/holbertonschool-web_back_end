15c9ce46efdf677b0ff9b5bc1fb165f07cbd76c8
jebeniani <aymenjebeniani@gmail.com>
2023-03-16 17:41:14 +0100

    creating a report object

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -3,7 +3,7 @@ export default function createReportObject(employeesList) {
     allEmployees: {
       ...employeesList,
     },
-    GetNumberOfDepartments(employeesList) {
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
