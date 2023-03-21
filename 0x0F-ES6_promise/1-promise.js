b9d6f08b58ecaab36b32c0a2f0fb6398dfc1c11e
jebeniani <aymenjebeniani@gmail.com>
2023-03-21 13:45:55 +0100

    u can't catch without a catch

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
