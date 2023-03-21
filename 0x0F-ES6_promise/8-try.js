360d2a00cf2033c9c8deb335b66df5fc06f0a8ef
jebeniani <aymenjebeniani@gmail.com>
2023-03-21 23:41:35 +0100

    divide function

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,7 +1,5 @@
 export default function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw new Error('Cannot divide by 0');
-  }
+  if (denominator === 0) throw new Error('Cannot divide by 0');
 
   return numerator / denominator;
 }
