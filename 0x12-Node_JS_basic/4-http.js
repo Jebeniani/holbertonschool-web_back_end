0b9a9bd9047535c1b7c04c4dc7311a25d5bda917
jebeniani <aymenjebeniani@gmail.com>
2023-04-03 20:04:13 +0100

    http server

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(1245);
+
+module.exports = app;
