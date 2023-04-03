bf338a8d40f9aa8e4206fe3c0e3cf674d3ac499b
jebeniani <aymenjebeniani@gmail.com>
2023-04-03 18:07:57 +0100

    use of process stdin

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
