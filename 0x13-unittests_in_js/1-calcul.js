b9d7356e8b6fd9352a0d9222bd876688b8c9c6e2
jebeniani <aymenjebeniani@gmail.com>
2023-04-06 18:39:33 +0100

    eslint fix

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,20 +1,20 @@
 function calculateNumber(type, a, b) {
-    const roundedA = Math.round(a);
-    const roundedB = Math.round(b);
+  const numA = Number(a);
+  const numB = Number(b);
 
-    switch (type) {
-        case 'SUM':
-            return roundedA + roundedB;
-        case 'SUBTRACT':
-            return roundedA - roundedB;
-        case 'DIVIDE':
-            if (roundedB === 0) {
-                return 'Error';
-            }
-            return Math.round(roundedA / roundedB);
-        default:
-            return 'Error';
+  if (Number.isNaN(numA) || Number.isNaN(numB)) throw TypeError;
+
+  if (type === 'SUM') {
+    return (Math.round(numA) + Math.round(numB));
+  } if (type === 'SUBTRACT') {
+    return (Math.round(numA) - Math.round(numB));
+  } if (type === 'DIVIDE') {
+    if (Math.round(numB) === 0) {
+      return ('Error');
     }
+    return (Math.round(numA) / Math.round(numB));
+  }
+  throw TypeError;
 }
 
 module.exports = calculateNumber;
