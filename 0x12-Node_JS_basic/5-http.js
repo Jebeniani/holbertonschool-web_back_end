e288ad06cc0a8535bd4a76fae6c173a5ae2dd29d
jebeniani <aymenjebeniani@gmail.com>
2023-04-09 18:17:07 +0100

    small http servre

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,17 +4,25 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/') res.end('Hello Holberton School!');
+
   if (req.url === '/students') {
-    res.write('This is the list of our students\n');
     try {
       const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
+      res.statusCode = 200;
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${data.numberOfStudents}\n`);
+      for (const [field, students] of Object.entries(data.studentByField)) {
+        if (field !== 'undefined') {
+          res.write(`Number of students in ${field}: ${students.count}. List: ${students.list.join(', ')}\n`);
+        }
+      }
+      res.end();
     } catch (error) {
+      res.statusCode = 404;
       res.end(error.message);
     }
   }
-  res.end();
 });
 app.listen(1245);
 module.exports = app;
