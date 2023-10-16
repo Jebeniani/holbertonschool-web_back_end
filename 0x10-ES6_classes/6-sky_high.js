6d6287f36826b3c5511bc012008d44df9bf24cf1
jebeniani <aymenjebeniani05@gmail.com>
2023-10-16 15:44:39 +0100

    inheritance

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -2,7 +2,7 @@ import Building from './5-building';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Call the parent class constructor
+    super(sqft);
     this._floors = floors;
   }
 
@@ -10,7 +10,6 @@ class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  // Override the method and provide a new implementation
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors.`;
   }
