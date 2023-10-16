b5f82db926361906d9246da4af699934d85ccf18
jebeniani <aymenjebeniani05@gmail.com>
2023-10-16 13:44:23 +0100

    eslint fix

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,36 +1,36 @@
 class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(newCode) {
-        if (typeof newCode === 'string') {
-            this._code = newCode;
-        } else {
-            throw new TypeError('Code must be a string');
-        }
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new TypeError('Code must be a string');
     }
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        } else {
-            throw new TypeError('Name must be a string');
-        }
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
+  }
 
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
 
 export default Currency;
