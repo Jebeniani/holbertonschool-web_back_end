fe7b145e62197143defb0cd4b795b777be4ee9e0
jebeniani <aymenjebeniani05@gmail.com>
2023-10-16 14:59:46 +0100

    building

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+/* eslint-disable class-methods-use-this */
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
