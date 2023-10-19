9d3eb1469b53d1119f887616c4b0913cccc69ea8
jebeniani <aymenjebeniani05@gmail.com>
2023-10-19 14:32:58 +0100

    http server using express

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.get('/students', async (req, res) => {
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
+});
+
+app.listen(port);
+module.exports = app;
