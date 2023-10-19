4a6fc4f65d5cba9366f839fbad6bec43bfef4638
jebeniani <aymenjebeniani05@gmail.com>
2023-10-19 13:48:29 +0100

    http server with express

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -3,15 +3,9 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-// Définissez une route pour l'endpoint /
 app.get('/', (req, res) => {
-  res.send('Bonjour Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-// Écoutez le serveur sur le port 1245
-app.listen(port, () => {
-  console.log(`Le serveur Express écoute sur le port ${port}`);
-});
-
-// Exportez l'application Express
+app.listen(port);
 module.exports = app;
